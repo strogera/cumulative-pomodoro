fa4c7b15481cb28c7c074a73cab04b2ee819643d
strogera <26325577+strogera@users.noreply.github.com>
2020-08-03 17:45:00 +0300

    improvements, one chronometer instead of two

--- app/src/main/java/com/example/cumulative_pomodoro/ChronometerActivity.java
+++ app/src/main/java/com/example/cumulative_pomodoro/ChronometerActivity.java
@@ -10,10 +10,22 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.TimeUnit;
+
 public class ChronometerActivity extends AppCompatActivity {
-    private long breakDuration=0;
+    private long breakDuration=0, accumulatedBreak=0;
     private enum State {PAUSE, WORK, BREAK};
     private State state= State.PAUSE;
+
+    private String formatMillistoMMSS(long t){
+        long minutes = Math.abs(t) /(1000 * 60);
+        long seconds = Math.abs(t) / 1000 % 60;
+        if(t<0){
+            return String.format("-%02d:%02d", Math.abs(minutes), Math.abs(seconds));
+        }else{
+            return String.format("%02d:%02d", minutes, seconds);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +34,18 @@ public class ChronometerActivity extends AppCompatActivity {
 
         final TextView currentMode =  (TextView)findViewById(R.id.CurrentMode);
         final Chronometer chronometer = (Chronometer)findViewById(R.id.Timer);
-        final Chronometer breakChronometer = (Chronometer)findViewById(R.id.BreakTimer);
+        final TextView breakChronometer = (TextView)findViewById(R.id.BreakTimer);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            breakChronometer.setCountDown(true);
+           // breakChronometer.setCountDown(true);
         }
         chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
-                long systemCurrTime = SystemClock.elapsedRealtime();
-                long chronometerBaseTime = chronometer.getBase();
-                long deltaTime = systemCurrTime - chronometerBaseTime;
-                breakChronometer.setBase(breakDuration+(long)(deltaTime*0.2)+SystemClock.elapsedRealtime());
+                long deltaTime = SystemClock.elapsedRealtime() - chronometer.getBase();
+                if(state==State.WORK) {
+                    breakDuration = accumulatedBreak + (long) (deltaTime * 0.2);// + SystemClock.elapsedRealtime();
+                    breakChronometer.setText(formatMillistoMMSS(breakDuration));
+                }
             }
         });
         chronometer.setOnClickListener(new View.OnClickListener() {
@@ -40,28 +53,42 @@ public class ChronometerActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (state==State.WORK) {
                     chronometer.stop();
-                    breakChronometer.start();
+                    chronometer.setBase(breakDuration+SystemClock.elapsedRealtime());
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        chronometer.setCountDown(true);
+                    }
+                    chronometer.start();
+
                     state = State.BREAK;
                     currentMode.setText("Break");
+                    breakChronometer.setVisibility(View.INVISIBLE);
                 }
                 else if (state == State.BREAK){
-                    breakChronometer.stop();
-                    breakDuration = breakChronometer.getBase() - SystemClock.elapsedRealtime();
+                    chronometer.stop();
+                    state=State.WORK;
+                    accumulatedBreak=chronometer.getBase()-SystemClock.elapsedRealtime();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        chronometer.setCountDown(false);
+                    }
                     chronometer.setBase(SystemClock.elapsedRealtime());
                     chronometer.start();
-                    state=State.WORK;
+
                     currentMode.setText("Work");
+                    breakChronometer.setText(formatMillistoMMSS(accumulatedBreak));
+                    breakChronometer.setVisibility(View.VISIBLE);
 
                 }else if (state==State.PAUSE){
                     chronometer.setBase(SystemClock.elapsedRealtime());
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        chronometer.setCountDown(false);
+                    }
                     chronometer.start();
                     state=State.WORK;
+
                     currentMode.setText("Work");
-
-
+                    breakChronometer.setVisibility(View.VISIBLE);
+                    breakDuration=0;
                 }
-
-
             }
         });
 
@@ -69,17 +96,18 @@ public class ChronometerActivity extends AppCompatActivity {
         buttonReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(state==State.WORK)
-                  chronometer.stop();
-                else if (state==State.BREAK)
-                    breakChronometer.stop();
+                chronometer.stop();
                 chronometer.setBase(SystemClock.elapsedRealtime());
-                breakChronometer.setBase(SystemClock.elapsedRealtime());
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    chronometer.setCountDown(false);
+                }
+                breakChronometer.setText(formatMillistoMMSS(0));
                 breakDuration=0;
+                accumulatedBreak=0;
                 state=State.PAUSE;
+
                 currentMode.setText("Pause");
-
-
+                breakChronometer.setVisibility(View.VISIBLE);
             }
         });
 
