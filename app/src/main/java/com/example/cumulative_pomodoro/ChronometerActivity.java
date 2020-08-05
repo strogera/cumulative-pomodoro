df361dd21f0d64c718db2143c0887e906cef59dc
strogera <26325577+strogera@users.noreply.github.com>
2020-08-05 16:38:50 +0300

    fullscreen mode with double tap

--- app/src/main/java/com/example/cumulative_pomodoro/ChronometerActivity.java
+++ app/src/main/java/com/example/cumulative_pomodoro/ChronometerActivity.java
@@ -14,21 +14,30 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.provider.Settings;
+import android.text.Layout;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+
 public class ChronometerActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.cumulative-pomodoro.MESSAGE";
     private long breakDuration=0, accumulatedBreak=0;
     private long lastBreak=0;
     private enum State {PAUSE, WORK, BREAK};
     private State state= State.PAUSE;
     private int statisticsWorkCounter=0, statisticsBreakCounter=0, statisticsWorkTime=0, statisticsBreakTime=0;
+    private boolean isFullScreen=false;
 
     private String formatStatistics(){
         long avrWorkTime=0;
@@ -59,6 +68,36 @@ public class ChronometerActivity extends AppCompatActivity {
             return String.format("%s%02d:%02d", hoursStr, minutes, seconds);
         }
     }
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        isFullScreen=true;
+
+        final TextView sessionInfo = (TextView)findViewById(R.id.SessionInfo);
+        sessionInfo.setVisibility(View.INVISIBLE);
+        Button buttonReset = (Button)findViewById(R.id.buttonResetChronometer);
+        buttonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.VISIBLE);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        isFullScreen=false;
+
+        final TextView sessionInfo = (TextView)findViewById(R.id.SessionInfo);
+        sessionInfo.setVisibility(View.VISIBLE);
+        Button buttonReset = (Button)findViewById(R.id.buttonResetChronometer);
+        buttonReset.setVisibility(View.VISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +108,26 @@ public class ChronometerActivity extends AppCompatActivity {
         final Chronometer chronometer = (Chronometer)findViewById(R.id.Timer);
         final TextView breakChronometer = (TextView)findViewById(R.id.BreakTimer);
         final TextView sessionInfo = (TextView)findViewById(R.id.SessionInfo);
+        final ConstraintLayout layout= (ConstraintLayout)findViewById(R.id.ConstraintLayout);
+
+        layout.setOnTouchListener(new ConstraintLayout.OnTouchListener() {
+            private GestureDetector gestureDetector = new GestureDetector(ChronometerActivity.this, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onDoubleTap(MotionEvent e) {
+                    if(isFullScreen)
+                        showSystemUI();
+                    else
+                        hideSystemUI();
+                    return super.onDoubleTap(e);
+                }
+            });
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gestureDetector.onTouchEvent(event);
+                return true;
+            }
+        });
 
         sessionInfo.setText(formatStatistics());
 
